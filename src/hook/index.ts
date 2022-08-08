3bc85de7a33bfaaf94cc91827f8f8b01f6321054
MIYABETaisei <mytspe46@gmail.com>
2022-08-08 22:53:40 +0900

    ✨ ABI

---
+++ src/hook/index.ts
@@ -0,0 +1,10 @@
+import { Contract, utils } from "ethers";
+import GourmetAbi from "../../constants/Gourmet.json";
+import { Gourmet } from "../../constants";
+
+const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const gourmetInterface = new utils.Interface(GourmetAbi.abi);
+export const contract = new Contract(
+  contractAddress,
+  gourmetInterface
+) as Gourmet;
