9543cd66c2dc4c3480a58620e312aa4925f711eb
MIYABETaisei <mytspe46@gmail.com>
2022-08-08 22:55:05 +0900

    ⚡️ usedapp対応

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
 import type { AppProps } from "next/app";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "../styles/globals.css";
+import { DAppProvider } from "@usedapp/core";
+
+const config = {
+  multicallAddresses: [
+    "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+  ],
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <DAppProvider config={config}>
+      <Component {...pageProps} />
+    </DAppProvider>
+  );
 }
 
 export default MyApp;
