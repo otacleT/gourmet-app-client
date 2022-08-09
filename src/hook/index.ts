bfbfcfdb5af42dc627485c43e7946cc469eca3ef
MIYABETaisei <mytspe46@gmail.com>
2022-08-09 12:10:57 +0900

    🚧 change contractAddress

--- src/hook/index.ts
+++ src/hook/index.ts
@@ -2,7 +2,7 @@ import { Contract, utils } from "ethers";
 import GourmetAbi from "../../constants/Gourmet.json";
 import { Gourmet } from "../../constants";
 
-const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const contractAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
 const gourmetInterface = new utils.Interface(GourmetAbi.abi);
 export const contract = new Contract(
   contractAddress,
