39032c6f31f458756cbdaeef3ba427021257ca6b
MIYABETaisei <mytspe46@gmail.com>
2022-08-10 14:59:50 +0900

    ⚡️ transactionNameを定義

--- src/hook/AddMap/AddMap.tsx
+++ src/hook/AddMap/AddMap.tsx
@@ -2,7 +2,9 @@ import { useContractFunction } from "@usedapp/core";
 import { contract } from "..";
 
 export const useAddMap = () => {
-  const { state, send } = useContractFunction(contract, "addMap");
+  const { state, send } = useContractFunction(contract, "addMap", {
+    transactionName: "Add",
+  });
   const loading =
     state.status === "PendingSignature" || state.status === "Mining";
   const success = state.status === "Success";
