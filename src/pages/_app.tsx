1b68c18e326ef16011c2acd7815dbdb8d8dbbbfe
MIYABETaisei <mytspe46@gmail.com>
2022-08-11 12:57:30 +0900

    🚧

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,10 @@ import "../styles/globals.css";
 import { DAppProvider, Hardhat } from "@usedapp/core";
 
 const config = {
-  // multicallAddresses: {
-  //   [Hardhat.chainId]: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
-  // },
-  multicallAddresses: ["0x5FbDB2315678afecb367f032d93F642f64180aa3"],
+  multicallAddresses: {
+    [Hardhat.chainId]: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+  },
+  // multicallAddresses: ["0x5FbDB2315678afecb367f032d93F642f64180aa3"],
 };
 
 function MyApp({ Component, pageProps }: AppProps) {
