5b328ab759fd27b584ceccc0cbbe4e425862ead0
MIYABETaisei <mytspe46@gmail.com>
2022-08-16 09:59:34 +0900

    🚧 コントラクトアドレス変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,22 @@ import type { AppProps } from "next/app";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "../styles/globals.css";
 import { DAppProvider, Hardhat } from "@usedapp/core";
+import Head from "next/head";
 
 const config = {
   multicallAddresses: {
-    [Hardhat.chainId]: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    [Hardhat.chainId]: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
   },
-  // multicallAddresses: ["0x5FbDB2315678afecb367f032d93F642f64180aa3"],
 };
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <DAppProvider config={config}>
+      <Head>
+        <title>Gourmet App</title>
+        <meta name="description" content="Gourmet App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </DAppProvider>
   );
