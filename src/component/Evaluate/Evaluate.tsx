3e0f2dff0a15768adf834987be7de66e2f2d4d40
MIYABETaisei <mytspe46@gmail.com>
2022-08-20 21:52:39 +0900

    ♻️ 評価コンポーネントを作成

--- src/component/Evaluate/Evaluate.tsx
+++ src/component/Evaluate/Evaluate.tsx
@@ -14,14 +14,24 @@ export const Evaluate: FC<Ev> = (props) => {
     <Drawer
       opened={elt}
       onClose={() => setElt(false)}
-      title="評価を行う"
+      title={ev?.name}
       padding="xl"
       size="lg"
       overlayOpacity={0.1}
       position="right"
       className="h-[calc(100vh-70px)] top-auto bottom-0"
     >
-      <h3>{ev?.name}</h3>
+      <p>{ev?.category}</p>
+      <p>{ev?.address}</p>
+      <div className="flex items-center justify-center rounded-sm bg-[#efefef] py-5 px-3">
+        <p className="text-2xl mr-4 leading-[1em] text-[#c9171e]">4.4/5</p>
+        <div className="relative w-[5em] h-[1em] text-3xl leading-[1em]">
+          <div className="absolute top-0 left-0 overflow-hidden whitespace-nowrap text-[#c9171e] w-[1.4em]">
+            ★★★★★
+          </div>
+          <div className="text-[#aeaeae]">☆☆☆☆☆</div>
+        </div>
+      </div>
       <ul>
         <li>{ev?.latitude}</li>
         <li>{ev?.longitude}</li>
