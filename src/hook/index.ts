ab8ec19371e2abb4527ecbc50804a5fb9451fae5
MIYABETaisei <mytspe46@gmail.com>
2022-08-21 11:30:15 +0900

    🚧 コントラクトアドレスの変更

--- src/hook/index.ts
+++ src/hook/index.ts
@@ -2,7 +2,7 @@ import { Contract, utils } from "ethers";
 import GourmetAbi from "../../contract/Gourmet.json";
 import { Gourmet } from "../../contract";
 
-const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 const gourmetInterface = new utils.Interface(GourmetAbi.abi);
 export const contract = new Contract(
   contractAddress,
