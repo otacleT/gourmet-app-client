1a41110a6f2c0a6ab2303785643662741513ffb7
MIYABETaisei <mytspe46@gmail.com>
2022-08-21 23:06:24 +0900

    🚧 コントラクトアドレスの変更

--- src/hook/index.ts
+++ src/hook/index.ts
@@ -2,7 +2,7 @@ import { Contract, utils } from "ethers";
 import GourmetAbi from "../../contract/Gourmet.json";
 import { Gourmet } from "../../contract";
 
-const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
 const gourmetInterface = new utils.Interface(GourmetAbi.abi);
 export const contract = new Contract(
   contractAddress,
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ import { Header } from "../component/Header";
 
 const config = {
   multicallAddresses: {
-    [Hardhat.chainId]: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+    [Hardhat.chainId]: "0x5fbdb2315678afecb367f032d93f642f64180aa3",
   },
 };
 
