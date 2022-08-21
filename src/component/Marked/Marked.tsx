f2c6fc628af914d2c18b539156003a18bad42475
MIYABETaisei <mytspe46@gmail.com>
2022-08-21 21:38:20 +0900

    🎨 markerの設置エラーの解決

--- src/component/Marked/Marked.tsx
+++ src/component/Marked/Marked.tsx
@@ -1,80 +1,15 @@
 import { Drawer } from "@mantine/core";
-import { FC, MutableRefObject, useCallback, useEffect, useState } from "react";
-import mapboxgl from "mapbox-gl";
-import { useShops } from "src/hook/Shops";
+import { Dispatch, FC, SetStateAction } from "react";
+import { Info } from "src/pages";
 
-type Marker = {
-  map: MutableRefObject<any>;
-};
-type Info = {
-  name: string;
-  category: string;
-  address: string;
-  latitude: number;
-  longitude: number;
+type Props = {
+  info: Info | undefined;
+  opened: boolean;
+  setOpened: Dispatch<SetStateAction<boolean>>;
 };
 
-export const Marked: FC<Marker> = (props) => {
-  const { map } = props;
-  const [opened, setOpened] = useState<boolean>(false);
-  const [info, setInfo] = useState<Info>();
-  const { isLoading, shops } = useShops();
-  const geojson = {
-    type: "Feature",
-    features: shops.map((shop) => ({
-      properties: {
-        name: shop.name,
-        category: shop.category,
-        postcode: shop.postcode,
-        address: shop.address,
-      },
-      geometry: {
-        type: "Point",
-        coordinates: {
-          lat: shop.latitude,
-          lng: shop.longitude,
-        },
-      },
-    })),
-  };
-  const handleEv = useCallback((e: any) => {
-    setInfo((prevstate) => {
-      return {
-        ...prevstate,
-        name: e.properties.name,
-        category: e.properties.category,
-        address: e.properties.address,
-        latitude: e.geometry.coordinates.lat,
-        longitude: e.geometry.coordinates.lng,
-      };
-    });
-  }, []);
-  useEffect(() => {
-    map.current.on("load", () => {
-      geojson.features.forEach((marker) => {
-        const registedMarker = new mapboxgl.Marker({
-          color: "#c9171e",
-        })
-          .setLngLat(marker.geometry.coordinates)
-          .setPopup(
-            new mapboxgl.Popup({ offset: 40 }).setHTML(
-              `<p style="font-size: 15px; font-weight: bold; color: #c9171e;">${marker.properties.name}</p>
-              <p style="font-size: 13px; line-height: 1.3;">category</p>
-              <div style="position: relative; width: 5em; height: 1em; font-size: 15px;">
-                <div style="position: absolute; top:0; left: 0; overflow: hidden; white-space: nowrap; color: #c9171e; width: 1.4em;">★★★★★</div>
-                <div style="color: #aeaeae;">☆☆☆☆☆</div>
-              </div>
-              `
-            )
-          )
-          .addTo(map.current);
-        registedMarker.getElement().addEventListener("click", function () {
-          handleEv(marker);
-          setOpened(true);
-        });
-      });
-    });
-  }, [shops]);
+export const Marked: FC<Props> = (props) => {
+  const { info, opened, setOpened } = props;
   return (
     <Drawer
       opened={opened}
