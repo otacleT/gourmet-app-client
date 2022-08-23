73fbe9d1a5d510cd39cfd1002d694f5abbe9c6e4
MIYABETaisei <mytspe46@gmail.com>
2022-08-23 23:08:33 +0900

    🚧 コントラクトアドレスの変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ import { Header } from "../component/Header";
 
 const config = {
   multicallAddresses: {
-    [Hardhat.chainId]: "0x5fbdb2315678afecb367f032d93f642f64180aa3",
+    [Hardhat.chainId]: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
   },
 };
 
