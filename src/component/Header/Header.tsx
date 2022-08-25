fdf807739ba41a51301eadb352365eaed6e63ccb
MIYABETaisei <mytspe46@gmail.com>
2022-08-25 14:56:36 +0900

    💄 TOPページ

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -5,21 +5,21 @@ import { FC } from "react";
 export const Header: FC = () => {
   const { activateBrowserWallet, account } = useEthers();
   return (
-    <header className="w-full bg-[#333333]">
-      <div className=" h-[70px] px-5 flex justify-between items-center">
-        <p className="text-[20px] font-bold text-white">Gourmet APP</p>
+    <header className="w-full">
+      <div className="max-w-6xl mx-auto h-[70px] px-5 flex justify-between items-center">
+        <p className="text-[20px] font-bold">Gourmet APP</p>
         <div className="flex justify-between items-center">
           {account ? (
             <div
-              className="text-sm cursor-pointer text-[#efefef] bg-inherit py-2 px-2 mr-5 rounded-sm"
-              style={{ border: "1px solid #57606a" }}
+              className="text-md cursor-pointer text-[#efefef] bg-[#55c08f] py-2 px-3 mr-5 rounded-md"
+              // style={{ border: "1px solid #57606a" }}
             >
               Connected
             </div>
           ) : (
             <button
-              className="text-sm cursor-pointer text-[#efefef] bg-inherit py-2 px-2 mr-5 rounded-sm"
-              style={{ border: "1px solid #57606a" }}
+              className="text-md cursor-pointer text-[#efefef] bg-[#55c08f] py-2 px-3 mr-5 rounded-md"
+              // style={{ border: "1px solid #57606a" }}
               onClick={activateBrowserWallet}
             >
               Connect wallet
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,34 +4,37 @@ import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
-    <main className="">
+    <main>
       <div className="w-full max-w-6xl px-5 pt-60 pb-80 mx-auto relative">
-        <p className="Heading relative table overflow-hidden">
+        {/* <p className="Heading relative table overflow-hidden">
           <span className="text-2xl py-2 px-3 inline-block text-white relative z-2 opacity-0 animate-textShow">
             テストタイトル
           </span>
-        </p>
-        <p className="Heading relative table overflow-hidden">
+        </p> */}
+        {/* <p className="Heading relative table overflow-hidden">
           <span className="text-2xl py-2 px-3 inline-block text-white relative z-2 opacity-0 animate-textShow">
             テスト
           </span>
-        </p>
-        <div className="flex w-60 justify-between mt-10">
+        </p> */}
+        <p className="text-2xl font-bold ">信頼度に応じた飲食店評価アプリ</p>
+        <div className="flex items-center mt-10">
           <Link href="/map">
-            <a className="w-[calc(50%-10px)] text-xl text-center py-1 px-5 bg-black text-white">
-              Guest
+            <a className="w-[150px] text-xl text-center py-3 px-4 rounded-lg bg-[#55c08f] text-white mr-10">
+              Login
             </a>
           </Link>
           <Link href="/map">
-            <a className="w-[calc(50%-10px)] text-xl text-center py-1 px-5 bg-black text-white">
-              Login
+            <a className="w-auto text-xl text-center py-1 border-b-2 border-black">
+              Guest start
             </a>
           </Link>
         </div>
         <div className="absolute right-1/3 top-[30px] z-1">
           <div className="w-[230px] h-[310px] relative">
-            <div className="Comment w-[145px] h-[80px] opacity-0 scale-50 flex items-center justify-center rounded-[50%] border-2 border-black bg-white absolute left-0 top-[30px] -translate-x-[15%] animate-comment01Show">
-              <p className="text-lg tracking-wider inline-block">Good!</p>
+            <div className="Comment w-[145px] h-[80px] opacity-0 scale-50 flex items-center justify-center rounded-[50%] border-2 border-[#333] bg-white absolute left-0 top-[30px] -translate-x-[15%] animate-comment01Show">
+              <p className="text-lg font-bold tracking-wider inline-block">
+                Good!
+              </p>
             </div>
             <div className="w-[80px] h-[200px] absolute right-0 bottom-0 text-center z-5">
               <Image
@@ -40,13 +43,15 @@ const Home: NextPage = () => {
                 layout="fill"
               />
             </div>
-            <div className="w-0 h-0 rounded-[50%] bg-[#aeaeae] absolute -right-[25px] -bottom-[10px] z-1 -translate-x-1/2 -translate-y-1/2 animate-circleShow"></div>
+            <div className="w-0 h-0 rounded-[50%] bg-[#55c08f] absolute -right-[25px] -bottom-[10px] z-1 -translate-x-1/2 -translate-y-1/2 animate-circleShow"></div>
           </div>
         </div>
         <div className="absolute right-1/3 bottom-[20px] z-2">
           <div className="w-[230px] h-[310px] relative">
-            <div className="Comment w-[145px] h-[80px] opacity-0 scale-50 flex items-center justify-center rounded-[50%] border-2 border-black bg-white absolute left-0 top-[30px] -translate-x-[15%] animate-comment01Show">
-              <p className="text-lg tracking-wider inline-block">Yummy!</p>
+            <div className="Comment w-[145px] h-[80px] opacity-0 scale-50 flex items-center justify-center rounded-[50%] border-2 border-[#333] bg-white absolute left-0 top-[30px] -translate-x-[15%] animate-comment01Show">
+              <p className="text-lg font-bold tracking-wider inline-block">
+                Yummy!
+              </p>
             </div>
             <div className="w-[90px] h-[200px] absolute right-0 bottom-0 text-center z-5">
               <Image
@@ -55,13 +60,15 @@ const Home: NextPage = () => {
                 layout="fill"
               />
             </div>
-            <div className="w-0 h-0 rounded-[50%] bg-[#aeaeae] absolute -right-[20px] -bottom-[10px] z-1 -translate-x-1/2 -translate-y-1/2 animate-circleShow"></div>
+            <div className="w-0 h-0 rounded-[50%] bg-[#55c08f] absolute -right-[20px] -bottom-[10px] z-1 -translate-x-1/2 -translate-y-1/2 animate-circleShow"></div>
           </div>
         </div>
         <div className="absolute right-[40px] bottom-1/4 z-3">
           <div className="w-[230px] h-[310px] relative">
-            <div className="Comment w-[145px] h-[80px] opacity-0 scale-50 flex items-center justify-center rounded-[50%] border-2 border-black bg-white absolute left-0 top-[30px] -translate-x-[15%] animate-comment01Show">
-              <p className="text-lg tracking-wider inline-block">Nice!</p>
+            <div className="Comment w-[145px] h-[80px] opacity-0 scale-50 flex items-center justify-center rounded-[50%] border-2 border-[#333] bg-white absolute left-0 top-[30px] -translate-x-[15%] animate-comment01Show">
+              <p className="text-lg font-bold tracking-wider inline-block">
+                Nice!
+              </p>
             </div>
             <div className="w-[86px] h-[200px] absolute right-0 bottom-0 text-center z-5">
               <Image
@@ -70,7 +77,7 @@ const Home: NextPage = () => {
                 layout="fill"
               />
             </div>
-            <div className="w-0 h-0 rounded-[50%] bg-[#aeaeae] absolute -right-[23px] -bottom-[10px] z-1 -translate-x-1/2 -translate-y-1/2 animate-circleShow"></div>
+            <div className="w-0 h-0 rounded-[50%] bg-[#55c08f] absolute -right-[23px] -bottom-[10px] z-1 -translate-x-1/2 -translate-y-1/2 animate-circleShow"></div>
           </div>
         </div>
       </div>
