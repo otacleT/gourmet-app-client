e98ad24c6d169e9fc81af3710ace38a2d3e331aa
MIYABETaisei <mytspe46@gmail.com>
2022-08-25 21:19:06 +0900

    ✨ firebaseの初期化

--- src/lib/firebase/init.ts
+++ src/lib/firebase/init.ts
@@ -1,6 +1,8 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 // FirebaseApp インスタンスを初期化する
-initializeApp({
+const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
@@ -8,4 +10,11 @@ initializeApp({
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-});
+};
+
+if (!getApps()?.length) {
+  initializeApp(firebaseConfig);
+}
+
+export const auth = getAuth();
+export const db = getFirestore();
