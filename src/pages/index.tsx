89173577961f03eaafea678741598862d5705b7c
MIYABETaisei <mytspe46@gmail.com>
2022-08-25 21:20:35 +0900

    ✨ Auth認証

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,25 @@
 import { NextPage } from "next";
 import Image from "next/image";
-import Link from "next/link";
+import { login, logout } from "src/lib/firebase/auth";
 
 const Home: NextPage = () => {
   return (
     <main>
       <div className="w-full max-w-6xl px-5 pt-60 pb-80 mx-auto relative">
-        {/* <p className="Heading relative table overflow-hidden">
-          <span className="text-2xl py-2 px-3 inline-block text-white relative z-2 opacity-0 animate-textShow">
-            テストタイトル
-          </span>
-        </p> */}
-        {/* <p className="Heading relative table overflow-hidden">
-          <span className="text-2xl py-2 px-3 inline-block text-white relative z-2 opacity-0 animate-textShow">
-            テスト
-          </span>
-        </p> */}
         <p className="text-2xl font-bold ">信頼度に応じた飲食店評価アプリ</p>
         <div className="flex items-center mt-10">
-          <Link href="/map">
-            <a className="w-[150px] text-xl text-center py-3 px-4 rounded-lg bg-[#55c08f] text-white mr-10">
-              Login
-            </a>
-          </Link>
-          <Link href="/map">
-            <a className="w-auto text-xl text-center py-1 border-b-2 border-black">
-              Guest start
-            </a>
-          </Link>
+          <button
+            className="w-[150px] text-xl text-center py-3 px-4 rounded-lg bg-[#55c08f] text-white mr-10"
+            onClick={login}
+          >
+            Login
+          </button>
+          <button
+            className="w-auto text-xl text-center py-1 border-b-2 border-black"
+            onClick={logout}
+          >
+            Guest start
+          </button>
         </div>
         <div className="absolute right-1/3 top-[30px] z-1">
           <div className="w-[230px] h-[310px] relative">
@@ -43,7 +35,7 @@ const Home: NextPage = () => {
                 layout="fill"
               />
             </div>
-            <div className="w-0 h-0 rounded-[50%] bg-[#55c08f] absolute -right-[25px] -bottom-[10px] z-1 -translate-x-1/2 -translate-y-1/2 animate-circleShow"></div>
+            <div className="w-0 h-0 rounded-[50%] bg-[#aeaeae] absolute -right-[25px] -bottom-[10px] z-1 -translate-x-1/2 -translate-y-1/2 animate-circleShow"></div>
           </div>
         </div>
         <div className="absolute right-1/3 bottom-[20px] z-2">
@@ -60,7 +52,7 @@ const Home: NextPage = () => {
                 layout="fill"
               />
             </div>
-            <div className="w-0 h-0 rounded-[50%] bg-[#55c08f] absolute -right-[20px] -bottom-[10px] z-1 -translate-x-1/2 -translate-y-1/2 animate-circleShow"></div>
+            <div className="w-0 h-0 rounded-[50%] bg-[#aeaeae] absolute -right-[20px] -bottom-[10px] z-1 -translate-x-1/2 -translate-y-1/2 animate-circleShow"></div>
           </div>
         </div>
         <div className="absolute right-[40px] bottom-1/4 z-3">
@@ -77,7 +69,7 @@ const Home: NextPage = () => {
                 layout="fill"
               />
             </div>
-            <div className="w-0 h-0 rounded-[50%] bg-[#55c08f] absolute -right-[23px] -bottom-[10px] z-1 -translate-x-1/2 -translate-y-1/2 animate-circleShow"></div>
+            <div className="w-0 h-0 rounded-[50%] bg-[#aeaeae] absolute -right-[23px] -bottom-[10px] z-1 -translate-x-1/2 -translate-y-1/2 animate-circleShow"></div>
           </div>
         </div>
       </div>
