d486cd5a60a94e864665c53d20486c449b842e34
MIYABETaisei <mytspe46@gmail.com>
2022-08-26 15:13:48 +0900

    💄 位置調整、リンク追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,26 @@
 import { NextPage } from "next";
 import Image from "next/image";
+import Link from "next/link";
 import { login, logout } from "src/lib/firebase/auth";
 
 const Home: NextPage = () => {
   return (
     <main>
-      <div className="w-full h-screen max-w-6xl px-5 pt-60 mx-auto relative">
-        <p className="text-2xl font-bold ">信頼度に応じた飲食店評価アプリ</p>
+      <div className="w-full h-[calc(100vh-70px)] max-w-6xl px-5 pt-60 mx-auto relative">
+        <p className="text-3xl font-bold ">信頼度に応じた飲食店評価アプリ</p>
         <div className="flex items-center mt-10">
-          <button
-            className="w-[150px] text-xl text-center py-3 px-4 rounded-lg bg-[#55c08f] text-white mr-10"
-            onClick={login}
-          >
-            Login
-          </button>
-          <button
-            className="w-auto text-xl text-center py-1 border-b-2 border-black"
-            onClick={logout}
-          >
-            Guest start
-          </button>
+          <Link href="/login">
+            <a className="w-[150px] text-xl text-center py-3 px-4 rounded-lg bg-[#55c08f] text-white mr-10">
+              Login
+            </a>
+          </Link>
+          <Link href="/create-account">
+            <a className="w-auto text-xl text-center py-1 border-b-2 border-black">
+              Guest start
+            </a>
+          </Link>
         </div>
-        <div className="absolute right-1/3 top-[30px] z-1">
+        <div className="absolute right-[32%] top-[50px] z-1">
           <div className="w-[230px] h-[310px] relative">
             <div className="Comment w-[145px] h-[80px] opacity-0 scale-50 flex items-center justify-center rounded-[50%] border-2 border-[#333] bg-white absolute left-0 top-[30px] -translate-x-[15%] animate-comment01Show">
               <p className="text-lg font-bold tracking-wider inline-block">
@@ -38,9 +37,9 @@ const Home: NextPage = () => {
             <div className="w-0 h-0 rounded-[50%] bg-[#aeaeae] absolute -right-[25px] -bottom-[10px] z-1 -translate-x-1/2 -translate-y-1/2 animate-circleShow"></div>
           </div>
         </div>
-        <div className="absolute right-1/3 bottom-1/4 z-2">
+        <div className="absolute right-1/3 bottom-[15%] z-2">
           <div className="w-[230px] h-[310px] relative">
-            <div className="Comment w-[145px] h-[80px] opacity-0 scale-50 flex items-center justify-center rounded-[50%] border-2 border-[#333] bg-white absolute left-0 top-[30px] -translate-x-[15%] animate-comment01Show">
+            <div className="Comment w-[145px] h-[80px] opacity-0 scale-50 flex items-center justify-center rounded-[50%] border-2 border-[#333] bg-white absolute -left-[5px] top-[10px] -translate-x-[15%] animate-comment01Show">
               <p className="text-lg font-bold tracking-wider inline-block">
                 Yummy!
               </p>
@@ -55,9 +54,9 @@ const Home: NextPage = () => {
             <div className="w-0 h-0 rounded-[50%] bg-[#aeaeae] absolute -right-[20px] -bottom-[10px] z-1 -translate-x-1/2 -translate-y-1/2 animate-circleShow"></div>
           </div>
         </div>
-        <div className="absolute right-[50px] bottom-[40%] z-3">
+        <div className="absolute right-[50px] bottom-[30%] z-3">
           <div className="w-[230px] h-[310px] relative">
-            <div className="Comment w-[145px] h-[80px] opacity-0 scale-50 flex items-center justify-center rounded-[50%] border-2 border-[#333] bg-white absolute left-0 top-[30px] -translate-x-[15%] animate-comment01Show">
+            <div className="Comment w-[145px] h-[80px] opacity-0 scale-50 flex items-center justify-center rounded-[50%] border-2 border-[#333] bg-white absolute -left-[5px] top-[10px] -translate-x-[15%] animate-comment01Show">
               <p className="text-lg font-bold tracking-wider inline-block">
                 Nice!
               </p>
