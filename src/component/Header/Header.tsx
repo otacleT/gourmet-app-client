17c9135701275a63dfaa7eb432a4fcfc171e2439
MIYABETaisei <mytspe46@gmail.com>
2022-08-26 18:04:29 +0900

    🚧 UIの表示切替

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,10 +1,19 @@
-import { Avatar } from "@mantine/core";
+import { Avatar, Menu, Text } from "@mantine/core";
 import { useEthers } from "@usedapp/core";
 import Link from "next/link";
-import { FC } from "react";
+import { useRouter } from "next/router";
+import { FC, useCallback } from "react";
+import { useAuth } from "src/context/auth";
+import { login, logout } from "src/lib/firebase/auth";
 
 export const Header: FC = () => {
   const { activateBrowserWallet, account } = useEthers();
+  const { fbUser } = useAuth();
+  const router = useRouter();
+  const handleLogout = useCallback(async () => {
+    await logout();
+    router.push("/");
+  }, []);
   return (
     <header className="w-full">
       <div className="max-w-6xl mx-auto h-[70px] px-5 flex justify-between items-center">
@@ -12,6 +21,20 @@ export const Header: FC = () => {
           <a className="text-2xl font-bold">Gourmet APP</a>
         </Link>
         <div className="flex justify-between items-center">
+          {fbUser ? (
+            <button
+              className="text-sm cursor-pointer text-[#efefef] bg-[#55c08f] py-2 px-3 mr-5 rounded-md"
+              onClick={logout}
+            >
+              Logout
+            </button>
+          ) : (
+            <Link href="/login">
+              <a className="text-sm cursor-pointer text-[#efefef] bg-[#55c08f] py-2 px-3 mr-5 rounded-md">
+                Login
+              </a>
+            </Link>
+          )}
           {account ? (
             <div
               className="text-md cursor-pointer text-[#efefef] bg-[#55c08f] py-2 px-3 mr-5 rounded-md"
@@ -28,7 +51,21 @@ export const Header: FC = () => {
               Connect wallet
             </button>
           )}
-          <Avatar radius="xl" />
+          {fbUser ? (
+            <Menu shadow="md" width={200}>
+              <Menu.Target>
+                <Avatar src={fbUser.photoURL} radius="xl" />
+              </Menu.Target>
+              <Menu.Dropdown>
+                <Menu.Label>Menu</Menu.Label>
+                <Menu.Item>My page</Menu.Item>
+                <Menu.Divider />
+                <Menu.Item color="red">Logout</Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
+          ) : (
+            <Avatar radius="xl" />
+          )}
         </div>
       </div>
     </header>
