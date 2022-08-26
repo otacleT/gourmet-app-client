442592449400caacb8dc73c6d92ab099728ee757
MIYABETaisei <mytspe46@gmail.com>
2022-08-26 18:01:34 +0900

    ⚡️ onSnapshot周りの改善

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -1,4 +1,9 @@
-import { onAuthStateChanged } from "firebase/auth";
+import {
+  onAuthStateChanged,
+  Unsubscribe,
+  User as FirebaseUser,
+} from "firebase/auth";
+import { doc, onSnapshot } from "firebase/firestore";
 import {
   createContext,
   ReactNode,
@@ -6,32 +11,51 @@ import {
   useEffect,
   useState,
 } from "react";
-import { auth } from "src/lib/firebase/init";
+import { auth, db } from "src/lib/firebase/init";
+import { User } from "src/types/user";
 
 type ContextType = {
-  isLoggedIn: boolean;
+  fbUser: FirebaseUser | null | undefined;
   isLoading: boolean;
+  user: User | null | undefined;
 };
 
 const AuthContext = createContext<ContextType>({
-  isLoggedIn: false,
+  fbUser: undefined,
   isLoading: true,
+  user: undefined,
 });
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>();
   const [isLoading, setIsLoading] = useState(true);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [fbUser, setFbUser] = useState<FirebaseUser | null>();
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      setIsLoggedIn(!!user);
-      setIsLoading(false);
+    let unsubribe: Unsubscribe;
+
+    onAuthStateChanged(auth, (resultUser) => {
+      unsubribe?.();
+      setFbUser(resultUser);
+
+      if (resultUser) {
+        setIsLoading(true);
+        const ref = doc(db, `users/${resultUser.uid}`);
+        unsubribe = onSnapshot(ref, (snap) => {
+          setUser(snap.data() as User);
+          setIsLoading(false);
+        });
+      } else {
+        setUser(null);
+        setIsLoading(false);
+      }
     });
   }, []);
   return (
     <AuthContext.Provider
       value={{
-        isLoggedIn,
+        fbUser,
         isLoading,
+        user,
       }}
     >
       {children}
