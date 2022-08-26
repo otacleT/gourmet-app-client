a38672b4a9298048e006b1c044b32d5f009ffe54
MIYABETaisei <mytspe46@gmail.com>
2022-08-26 11:48:14 +0900

    💄 トップページ調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { login, logout } from "src/lib/firebase/auth";
 const Home: NextPage = () => {
   return (
     <main>
-      <div className="w-full max-w-6xl px-5 pt-60 pb-80 mx-auto relative">
+      <div className="w-full h-screen max-w-6xl px-5 pt-60 mx-auto relative">
         <p className="text-2xl font-bold ">信頼度に応じた飲食店評価アプリ</p>
         <div className="flex items-center mt-10">
           <button
@@ -28,7 +28,7 @@ const Home: NextPage = () => {
                 Good!
               </p>
             </div>
-            <div className="w-[80px] h-[200px] absolute right-0 bottom-0 text-center z-5">
+            <div className="w-[80px] h-[220px] absolute right-0 bottom-0 text-center z-5">
               <Image
                 className="object-contain z-10 opacity-0 translate-y-[20px] animate-peopleShow"
                 src="/people01.png"
@@ -38,14 +38,14 @@ const Home: NextPage = () => {
             <div className="w-0 h-0 rounded-[50%] bg-[#aeaeae] absolute -right-[25px] -bottom-[10px] z-1 -translate-x-1/2 -translate-y-1/2 animate-circleShow"></div>
           </div>
         </div>
-        <div className="absolute right-1/3 bottom-[20px] z-2">
+        <div className="absolute right-1/3 bottom-1/4 z-2">
           <div className="w-[230px] h-[310px] relative">
             <div className="Comment w-[145px] h-[80px] opacity-0 scale-50 flex items-center justify-center rounded-[50%] border-2 border-[#333] bg-white absolute left-0 top-[30px] -translate-x-[15%] animate-comment01Show">
               <p className="text-lg font-bold tracking-wider inline-block">
                 Yummy!
               </p>
             </div>
-            <div className="w-[90px] h-[200px] absolute right-0 bottom-0 text-center z-5">
+            <div className="w-[90px] h-[235px] absolute right-0 bottom-0 text-center z-5">
               <Image
                 className="object-contain z-10 opacity-0 translate-y-[20px] animate-peopleShow"
                 src="/people02.png"
@@ -55,14 +55,14 @@ const Home: NextPage = () => {
             <div className="w-0 h-0 rounded-[50%] bg-[#aeaeae] absolute -right-[20px] -bottom-[10px] z-1 -translate-x-1/2 -translate-y-1/2 animate-circleShow"></div>
           </div>
         </div>
-        <div className="absolute right-[40px] bottom-1/4 z-3">
+        <div className="absolute right-[50px] bottom-[40%] z-3">
           <div className="w-[230px] h-[310px] relative">
             <div className="Comment w-[145px] h-[80px] opacity-0 scale-50 flex items-center justify-center rounded-[50%] border-2 border-[#333] bg-white absolute left-0 top-[30px] -translate-x-[15%] animate-comment01Show">
               <p className="text-lg font-bold tracking-wider inline-block">
                 Nice!
               </p>
             </div>
-            <div className="w-[86px] h-[200px] absolute right-0 bottom-0 text-center z-5">
+            <div className="w-[90px] h-[235px] absolute right-0 bottom-0 text-center z-5">
               <Image
                 className="object-contain z-10 opacity-0 translate-y-[20px] animate-peopleShow"
                 src="/people03.png"
