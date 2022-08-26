d42e16ffe499169f1376a2cb07de7867fc284fc9
MIYABETaisei <mytspe46@gmail.com>
2022-08-26 22:54:10 +0900

    ⚡️ リダイレクト設定

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,21 +2,21 @@ import { Button } from "@mantine/core";
 import { useRouter } from "next/router";
 import { FcGoogle } from "react-icons/fc";
 import { useAuth } from "src/context/auth";
-import { login, logout } from "src/lib/firebase/auth";
+import { login } from "src/lib/firebase/auth";
 
 const LoginPage = () => {
   const { user, fbUser, isLoading } = useAuth();
   const router = useRouter();
+  if (isLoading) {
+    return <div className="loading"></div>;
+  }
+  if (user) {
+    router.push("/map");
+    return null;
+  }
   if (fbUser) {
     router.push("/create-account");
     return null;
-  }
-  if (user) {
-    router.push("/");
-    return null;
-  }
-  if (isLoading) {
-    return <div className="loading"></div>;
   }
   return (
     <div className="max-w-xl mx-auto pt-20">
@@ -28,12 +28,6 @@ const LoginPage = () => {
       >
         Google
       </Button>
-      <button
-        className="w-full text-lg text-black border-2 border-[#efefef] h-14 hover:bg-[#efefef] mt-10"
-        onClick={logout}
-      >
-        Logout
-      </button>
     </div>
   );
 };
