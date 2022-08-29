92bd6cdaffb300f4f52f7596f408ee5d0075cbbc
MIYABETaisei <mytspe46@gmail.com>
2022-08-29 22:36:12 +0900

    ✨ firestoreへ評価情報の登録

---
+++ src/lib/firebase/evaluate.ts
@@ -0,0 +1,23 @@
+import { User } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "./init";
+
+export type Item = {
+  user: User | null | undefined;
+  name: string;
+  category: string;
+  address: string;
+  star: number;
+};
+
+export async function addList(item: Item): Promise<void> {
+  const addId = Math.round(Math.random() * 10000000000);
+  if (!item.user) return;
+  const ref = doc(db, `users/${item.user.uid}/list`, String(addId));
+  await setDoc(ref, {
+    name: item.name,
+    category: item.category,
+    address: item.address,
+    star: item.star,
+  });
+}
