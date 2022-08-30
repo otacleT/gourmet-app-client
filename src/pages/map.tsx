219accf70aaee74cfaafb6c222b0ba1f41fb6059
MIYABETaisei <mytspe46@gmail.com>
2022-08-30 23:00:23 +0900

    💄 marker色調整

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -105,7 +105,7 @@ const Map: NextPage = () => {
     map.current.on("load", () => {
       geojson.features.forEach((marker) => {
         const registedMarker = new mapboxgl.Marker({
-          color: "#DA382F",
+          color: "#fe553e",
         })
           .setLngLat(marker.geometry.coordinates)
           .addTo(map.current);
