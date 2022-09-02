48849c39608e68a563ddbb6d22217024797ddb6d
MIYABETaisei <mytspe46@gmail.com>
2022-09-02 14:46:10 +0900

    ✨ mapbox-glをreact-map-glへ変更

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,21 +1,22 @@
-import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import { FirebaseApp, getApp } from "firebase/app";
-import mapboxgl from "mapbox-gl";
 import { NextPage } from "next";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
+import ReactMap, {
+  GeolocateControl,
+  Marker,
+  NavigationControl,
+} from "react-map-gl";
 import { ShopInfo } from "src/component/ShopInfo";
-import { useResult } from "src/hook/Result";
-import { Result } from "src/hook/Result/Result";
+import { Result, useResult } from "src/hook/Result/Result";
 import { useShops } from "src/hook/Shops";
+import GeocoderControl from "src/lib/mapbox/geocoder";
 import { Info } from "src/types/info";
 import "../lib/firebase/init";
 
 const Map: NextPage = () => {
   const [info, setInfo] = useState<Info>();
   const [opened, setOpened] = useState<boolean>(false);
-  const mapContainer = useRef<any>(null);
-  const map = useRef<mapboxgl.Map | any>(null);
   const app: FirebaseApp = getApp();
   const { shops } = useShops();
   const { results } = useResult();
@@ -66,56 +67,41 @@ const Map: NextPage = () => {
       };
     });
   }, []);
-  useEffect(() => {
-    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN ?? "";
-    map.current = new mapboxgl.Map({
-      logoPosition: "bottom-left",
-      attributionControl: false,
-      container: mapContainer.current,
-      style: process.env.NEXT_PUBLIC_MAPBOX_STYLE,
-      center: [139.6503, 35.6762], // center map on Chad
-      zoom: 10,
-    });
-    const geocoder = new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl as any,
-      types: "poi",
-      marker: false,
-      placeholder: "seach a store",
-    });
-    const geolocate = new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true,
-      },
-      trackUserLocation: true,
-      showUserHeading: true,
-    });
-    const nav = new mapboxgl.NavigationControl({
-      visualizePitch: true,
-    });
-    map.current.addControl(geocoder, "top-left");
-    map.current.addControl(geolocate, "top-right");
-    map.current.addControl(nav, "top-right");
+  const handleMarker = useCallback((item: any) => {
+    handleInfo(item);
+    setOpened(true);
   }, []);
-
-  useEffect(() => {
-    map.current.on("load", () => {
-      geojson.features.forEach((marker) => {
-        const registedMarker = new mapboxgl.Marker({
-          color: "#fe553e",
-        })
-          .setLngLat(marker.geometry.coordinates)
-          .addTo(map.current);
-        registedMarker.getElement().addEventListener("click", function () {
-          handleInfo(marker);
-          setOpened(true);
-        });
-      });
-    });
-  }, [geojson]);
   return (
-    <main>
-      <div className="w-screen h-[calc(100vh-70px)]" ref={mapContainer} />
+    <main className="h-[calc(100vh-70px)]">
+      <ReactMap
+        initialViewState={{
+          longitude: 139.6503,
+          latitude: 35.6762,
+          zoom: 10,
+        }}
+        mapStyle={process.env.NEXT_PUBLIC_MAPBOX_STYLE}
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN}
+        attributionControl={false}
+      >
+        <GeocoderControl
+          mapboxAccessToken={
+            process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN ?? ""
+          }
+          position="top-left"
+          placeholder="search a store"
+        />
+        {geojson.features.map((marker) => (
+          <Marker
+            latitude={marker.geometry.coordinates.lat}
+            longitude={marker.geometry.coordinates.lng}
+            onClick={() => handleMarker(marker)}
+            color="#fe553e"
+            key={Math.round(Math.random() * 10000)}
+          />
+        ))}
+        <GeolocateControl />
+        <NavigationControl />
+      </ReactMap>
       <ShopInfo info={info} opened={opened} setOpened={setOpened} />
     </main>
   );
