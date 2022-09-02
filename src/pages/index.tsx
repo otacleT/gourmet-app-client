47d06e86b033d3897203016f3fd59a9f04f4b8a4
MIYABETaisei <mytspe46@gmail.com>
2022-09-02 15:24:08 +0900

    💄 位置調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ const Home: NextPage = () => {
             </Link>
           )}
         </div>
-        <div className="absolute right-[35%] top-[40px] z-1">
+        <div className="absolute right-[33%] top-[40px] z-1">
           <div className="w-[230px] h-[310px] relative">
             <div className="Comment w-[145px] h-[80px] opacity-0 scale-50 flex items-center justify-center rounded-[50%] border-2 border-[#333] bg-white absolute left-0 top-[30px] -translate-x-[15%] animate-comment01Show">
               <p className="text-lg font-bold tracking-wider inline-block">
@@ -49,14 +49,14 @@ const Home: NextPage = () => {
             <div className="w-0 h-0 rounded-[50%] bg-[#2cb696] absolute right-[40px] bottom-[5px] z-1 translate-x-1/2 translate-y-1/2 animate-circleShow"></div>
           </div>
         </div>
-        <div className="absolute right-[35%] bottom-[15%] z-2">
+        <div className="absolute right-[37%] bottom-[15%] z-2">
           <div className="w-[230px] h-[310px] relative">
             <div className="Comment w-[145px] h-[80px] opacity-0 scale-50 flex items-center justify-center rounded-[50%] border-2 border-[#333] bg-white absolute -left-[5px] top-[10px] -translate-x-[15%] animate-comment01Show">
               <p className="text-lg font-bold tracking-wider inline-block">
                 Yummy!
               </p>
             </div>
-            <div className="w-[90px] h-[240px] absolute right-0 bottom-0 text-center z-5">
+            <div className="w-[90px] h-[250px] absolute right-0 bottom-0 text-center z-5">
               <Image
                 className="object-contain z-10 opacity-0 translate-y-[20px] animate-peopleShow"
                 src="/people02.png"
@@ -66,7 +66,7 @@ const Home: NextPage = () => {
             <div className="w-0 h-0 rounded-[50%] bg-[#2cb696] absolute right-[45px] bottom-[5px] z-1 translate-x-1/2 translate-y-1/2 animate-circleShow"></div>
           </div>
         </div>
-        <div className="absolute right-[50px] bottom-[30%] z-3">
+        <div className="absolute right-[60px] bottom-[30%] z-3">
           <div className="w-[230px] h-[310px] relative">
             <div className="Comment w-[145px] h-[80px] opacity-0 scale-50 flex items-center justify-center rounded-[50%] border-2 border-[#333] bg-white absolute -left-[5px] top-[10px] -translate-x-[15%] animate-comment01Show">
               <p className="text-lg font-bold tracking-wider inline-block">
