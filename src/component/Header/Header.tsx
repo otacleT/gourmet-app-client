daba842cbeb1709256010490ad34b3234b4a25d3
MIYABETaisei <mytspe46@gmail.com>
2022-09-03 14:48:44 +0900

    ♻️ ヘッダーメニューをコンポーネントへ変更

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,10 +1,8 @@
-import { Avatar, Menu } from "@mantine/core";
 import Link from "next/link";
 import { FC, useState } from "react";
-import { IoIosArrowDown } from "react-icons/io";
 import { useAuth } from "src/context/auth";
-import { logout } from "src/lib/firebase/auth";
 import { MyProfile } from "../MyProfile";
+import { UserIcon } from "../UserIcon";
 import { WalletConnect } from "../WalletConnect";
 
 export const Header: FC = () => {
@@ -18,15 +16,8 @@ export const Header: FC = () => {
           <a className="text-2xl font-bold">Gourmet APP</a>
         </Link>
         <div className="flex justify-between items-center">
-          {!fbUser && (
-            <Link href="/login">
-              <a className="text-sm leading-none cursor-pointer font-bold text-white bg-[#2cb696] p-3 mr-5 rounded-md">
-                ログイン
-              </a>
-            </Link>
-          )}
-          {fbUser &&
-            (user ? (
+          {fbUser ? (
+            user ? (
               <WalletConnect />
             ) : (
               <Link href="/create-account">
@@ -34,34 +25,16 @@ export const Header: FC = () => {
                   アカウント作成
                 </a>
               </Link>
-            ))}
-
-          {fbUser ? (
-            <Menu shadow="md" width={200}>
-              <Menu.Target>
-                <div className="relative">
-                  <Avatar
-                    src={fbUser.photoURL}
-                    radius="xl"
-                    className="shadow-md shadow-[#aeaeae]"
-                  />
-                  <IoIosArrowDown className="w-[11px] h-[11px] absolute bottom-0 right-0 z-5 rounded-full bg-white shadow-sm shadow-black" />
-                </div>
-              </Menu.Target>
-              <Menu.Dropdown>
-                <Menu.Label>メニュー</Menu.Label>
-                <Menu.Item onClick={() => setIsMypage(true)}>
-                  アカウント情報
-                </Menu.Item>
-                <Menu.Divider />
-                <Menu.Item color="red" onClick={logout}>
-                  ログアウト
-                </Menu.Item>
-              </Menu.Dropdown>
-            </Menu>
+            )
           ) : (
-            <Avatar radius="xl" />
+            <Link href="/login">
+              <a className="text-sm leading-none cursor-pointer font-bold text-white bg-[#2cb696] p-3 mr-5 rounded-md">
+                ログイン
+              </a>
+            </Link>
           )}
+
+          <UserIcon setIsMypage={setIsMypage} />
         </div>
       </div>
       <MyProfile isMypage={isMypage} setIsMypage={setIsMypage} />
