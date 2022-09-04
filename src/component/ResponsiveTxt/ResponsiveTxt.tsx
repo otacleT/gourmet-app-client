45bed173df06798c68809960c9300d74d11e2c5f
MIYABETaisei <mytspe46@gmail.com>
2022-09-04 17:42:37 +0900

    ✨ 768px以下はテキスト表示

---
+++ src/component/ResponsiveTxt/ResponsiveTxt.tsx
@@ -0,0 +1,7 @@
+export const ResponsiveTxt = () => {
+  return (
+    <p className="w-full px-2 block md:hidden text-lg text-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      768px以上の端末でのみご利用いただけます。
+    </p>
+  );
+};
