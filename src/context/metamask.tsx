a5ba8534aefe0e0754c9eeaa1a810d75e362ba93
MIYABETaisei <mytspe46@gmail.com>
2022-09-04 22:46:30 +0900

    ⚡️ 確認中のstateを追加

--- src/context/metamask.tsx
+++ src/context/metamask.tsx
@@ -8,22 +8,26 @@ import {
 
 type ContextType = {
   hasMetamask: boolean;
+  isStarting: boolean;
 };
 
 const MetamaskContext = createContext<ContextType>({
   hasMetamask: false,
+  isStarting: true,
 });
 
 export const MetamaskProvider = ({ children }: { children: ReactNode }) => {
   const [hasMetamask, setHasMetamask] = useState<boolean>(false);
+  const [isStarting, setIsStarting] = useState<boolean>(true);
 
   useEffect(() => {
     if (window.ethereum && window.ethereum.isMetaMask) {
       setHasMetamask(true);
     }
+    setIsStarting(false);
   }, []);
   return (
-    <MetamaskContext.Provider value={{ hasMetamask }}>
+    <MetamaskContext.Provider value={{ hasMetamask, isStarting }}>
       {children}
     </MetamaskContext.Provider>
   );
